3367d5eb24b939f0eca7bdc40b9922d32346d775
ThomasCapelli <thomas.capelli@studo.unibo.it>
2021-12-08 12:12:03 +0100

    fixed the sticky nav and the carousel. Added new icons ancd changed the style of the main menu

--- js/sticky-nav.js
+++ js/sticky-nav.js
@@ -2,13 +2,16 @@ $(document).ready(function(){
     window.onscroll = function() {StickNav()};
     var navbar = document.getElementById("stickyNav");
     var sticky = navbar.offsetTop;
+    
 
     function StickNav() {
         console.log(sticky);
         if (window.pageYOffset >= sticky) {
             navbar.classList.add("sticky")
+            $("main").addClass("content");
         } else {
             navbar.classList.remove("sticky");
+            $("main").removeClass("content");
         }
     }
 });
