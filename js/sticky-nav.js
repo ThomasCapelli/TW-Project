7db8018d982fa7211d700ac8db1e36d2866ffe64
ThomasCapelli <thomas.capelli@studio.ubnibo.it>
2021-12-09 09:31:05 +0100

    temporary complete style of the sticky navbar and the js code to make it work

--- js/sticky-nav.js
+++ js/sticky-nav.js
@@ -1,8 +1,16 @@
+function selected(element) {
+    $(element).addClass("selected");
+    $(element).nextAll().removeClass("selected");
+    $(element).prevAll().removeClass("selected");
+}
 $(document).ready(function(){
     window.onscroll = function() {StickNav()};
     var navbar = document.getElementById("stickyNav");
     var sticky = navbar.offsetTop;
-    
+    var links = document.querySelectorAll("body nav ul");
+    $("body nav ul li ").click(function(){
+        selected($(this));
+    });
 
     function StickNav() {
         if (window.pageYOffset >= sticky) {
