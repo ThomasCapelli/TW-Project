137fb0ef0cc759c647a1fd0c9a5a5c692506a374
Ciprian <ciprianstricescu@gmail.com>
2021-12-13 15:21:16 +0100

    Cleaning code

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -3,28 +3,9 @@ $(document).ready(function(){
         check = $("input[name='chk[]']").is(":checked");
         if(check){
             $("body").addClass("dark_mode").removeClass("light_mode");
-           /*  $(".icon").each(function(){
-                $(this).css("filter","invert(100%)");
-            }); */
-            /* //Invert icon colors
-            $(".icon").each(function(){
-                if($(this).css("filter")=="0%" || $(this).css("filter")=="none"){
-                    $(this).css("filter","invert(100%)");
-                }
-                if($(this).css("filter")=="100%"){
-                    $(this).css("filter","none");
-                }
-            }); */
         }
         else{
             $("body").addClass("light_mode").removeClass("dark_mode");
-           /*  $(".icon").each(function(){
-                $(this).css("filter","");
-            });  */
-          /*   //Invert icon colors
-            $(".icon").each(function(){
-                $(this).css("filter","");
-            }); */
         }
     });     
 });
