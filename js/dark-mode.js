9f7c4092fcf84383963992d47c2dbd419c71c8f8
ThomasCapelli <thomas.capelli@studio.ubnibo.it>
2021-12-20 18:27:37 +0100

    cart button now appears when scrolling down

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,15 +1,12 @@
 function bind() {
     $("div.removable").click(function(){
+        $("div.removable").empty();
         if($("body").hasClass("light_mode")){
             $("body").addClass("dark_mode").removeClass("light_mode");
-            $("div.removable img").remove();
-            $("div.removable p").remove();
             $("div.removable").append('<img src="../Icons/candela_spenta.png" alt="Dark mode logo"/><p>dark mode</p>');
         }
         else{
             $("body").addClass("light_mode").removeClass("dark_mode");
-            $("div.removable img").remove();
-            $("div.removable p").remove();
             $("div.removable").append('<img src="../Icons/candela_accesa_colored.png" alt="Light Mode logo"/><p>light mode</p>');
         }
     });    
