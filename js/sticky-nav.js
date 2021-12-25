9d07b6e2b2073c4bc72dd22678197e1e9bb4fa6d
ThomasCapelli <thomas.capelli@studo.unibo.it>
2021-12-25 19:55:56 +0100

    fixed visual details

--- js/sticky-nav.js
+++ js/sticky-nav.js
@@ -1,24 +1,29 @@
-function selected(element) {
-    $(element).addClass("selected");
-    $(element).nextAll().removeClass("selected");
-    $(element).prevAll().removeClass("selected");
-}
-$(document).ready(function(){
-    window.onscroll = function() {StickNav()};
-    var navbar = document.getElementById("stickyNav");
-    var sticky = navbar.offsetTop;
-    $("body nav ul li ").click(function(){
-        selected($(this));
-    });
-    function StickNav() {
-        if (window.pageYOffset >= sticky) {
-            navbar.classList.add("sticky")
-            $("main").addClass("content");
-            $("#cartbutton").css("display", "initial");
-        } else {
-            navbar.classList.remove("sticky");
-            $("main").removeClass("content");
-            $("#cartbutton").css("display", "none");
+function selected() {
+    var item = document.querySelectorAll("body nav ul li a");
+    for(let i = 0; i < item.length; i++) {
+        if(item[i].href === location.href) {
+            $(item[i]).addClass("selected");
+            $(item[i]).nextAll().removeClass("selected");
+            $(item[i]).prevAll().removeClass("selected");
         }
     }
+}
+function StickNav(navbar, sticky) {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+        $("main").addClass("content");
+        $("#cartbutton").css("display", "initial");
+    } else {
+        navbar.classList.remove("sticky");
+        $("main").removeClass("content");
+        $("#cartbutton").css("display", "none");
+    }
+}
+$(document).ready(function(){
+    var navbar = document.getElementById("stickyNav");
+    var sticky = navbar.offsetTop;
+    window.onscroll = function() {
+        StickNav(navbar, sticky);
+    };
+    selected();
 });
