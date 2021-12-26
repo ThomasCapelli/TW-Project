01841b971b07730b7eb2f47befbb717a5c27076e
ThomasCapelli <thomas.capelli@studio.ubnibo.it>
2021-12-26 20:43:29 +0100

    login logic + style on error/correct

--- js/form.js
+++ js/form.js
@@ -1,15 +1,77 @@
 const pattern = /^(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$/i;
-$(document).ready(function(){
-    const password = $("#password");
-    password.keyup(function (e) { 
-        if(password.val()==undefined || password.val().length < 8 || !pattern.test(password.val())) {
-           if(!password.next().is("#error"))
-             {
-                password.parent().append("<p id='error'>La password deve essere almeno 8 caratteri e deve contenere almeno una lettera ed un numero</p>");
-             }
+function passwordCheck(password) {
+    if(password.val()===undefined || password.val().length < 8 || !pattern.test(password.val())) {
+        if(getError(password).val() === undefined)
+          {
+              password.addClass("isError");
+              password.parent().append("<p id='error'>Password non valida. La password deve contenere almeno 8 caratteri, 1 lettera maiuscola e 1 numero</p>");
+          }
+     }
+     else {
+         password.removeClass("isError");
+         getError(password).remove();
+     }
+}
+function passwordConfirmCheck(element) {
+    if(isValuePresent(element) && $("#password").val() != element.val()) {
+        if(getError(element).val() === undefined)
+        {
+            element.addClass("isError");
+            element.parent().append("<p id='error'>Le password inserite non corrispondono</p>");
         }
-        else {
-            password.next().remove();
+    }  
+    else {
+        element.removeClass("isError");
+        getError(element).remove();
+   }
+    
+}
+function isValuePresent(element) {
+    if(element.val().length > 0) {
+        return true;
+    }
+    return false;
+}
+function getError(element) {
+    return element.next().next().filter("#error");
+}
+function canEnable(input) {
+    let canEnable = true;
+    input.each(function() {
+        if($(this).prop('required') && !$(this).hasClass("isCorrect")) {
+            canEnable = false;
         }
     });
+    return canEnable;
+}
+$(document).ready(function(){
+    var input = $("main form input");
+    input.focus(function(){
+        $(this).next().addClass("moved");
+    });
+    input.focusout(function(){
+        if(!isValuePresent($(this))) {
+            $(this).next().removeClass("moved");
+        }
+    });
+    input.keyup(function(e) {
+        if($(this).is("#password")) {
+            passwordCheck($(this));
+            passwordConfirmCheck($("#confirm"));
+        } else if($(this).is("#confirm")) {
+            passwordConfirmCheck($(this));
+        }
+        if(isValuePresent($(this))) {
+            $(this).next().addClass("moved");
+            if(getError($(this)).val() === undefined) {
+                $(this).addClass("isCorrect");
+            } else {
+                $(this).removeClass("isCorrect");
+            }
+        } 
+        if(canEnable(input)) {
+            $("input[type=submit]").attr("disabled", false);
+        }
+    });
+    
 });
