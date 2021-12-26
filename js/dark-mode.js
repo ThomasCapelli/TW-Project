e1a04e9e574a9aeb7f3ba20aea90c14d3df5c229
ThomasCapelli <thomas.capelli@studo.unibo.it>
2021-12-26 23:58:36 +0100

    fixed the darkmode button

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,13 +1,12 @@
 function bind() {
-    $("div.removable").click(function(){
-        $("div.removable").empty();
+    $("li#light").click(function(){
         if($("body").hasClass("light_mode")){
             $("body").addClass("dark_mode").removeClass("light_mode");
-            $("div.removable").append('<img src="../Icons/candela_spenta.png" alt="Dark mode logo"/><p>dark mode</p>');
+            $("li#light a").empty().append('<span class="icon_A_nav"><img src="../icons/candela_spenta.png" alt="Dark mode logo"/></span><span class="text_A_nav">Dark mode</span>');
         }
         else{
             $("body").addClass("light_mode").removeClass("dark_mode");
-            $("div.removable").append('<img src="../Icons/candela_accesa_colored.png" alt="Light Mode logo"/><p>light mode</p>');
+            $("li#light a").empty().append('<span class="icon_A_nav"><img src="../icons/candle.png" alt="Light mode logo"/></span><span class="text_A_nav">Ligh mode</span>');
         }
     });    
 }
