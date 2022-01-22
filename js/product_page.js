b5b2e472a49b0512f94a6c0020a6fd5d2c0b5f09
Ciprian <ciprianstricescu@gmail.com>
2022-01-22 23:15:28 +0100

    new fix for cart and productpage

--- js/product_page.js
+++ js/product_page.js
@@ -9,49 +9,46 @@ function showSnackBar(testo) {
     }, 4000);
 }
 var cont=1;
+var flag=0;
 $(document).ready(function(){
     $('.addToCart').click(function() {
         window.$_GET = new URLSearchParams(location.search);
-        var activeColor = $(".colorName").text();
-        var activeSize = $("input[name='size']:checked").val();
-        console.log(activeSize);
-        var activeSizeQty =$("input[name='size']:checked").attr('id').split(" ");
-        var Qty = parseInt(activeSizeQty[1]);
-        var idProd = $_GET.get('productId');
-        var idCat = $_GET.get('categoryId');
-        var data;
-        var ajaxurl = '../php/order.php';
-        console.log(Qty);
-        if(Qty-cont==-1){
-            showSnackBar("Taglia e colore scelto non disponibile")
+        if(flag==1){
+            var activeColor = $(".colorName").text();
+            var size = $(".tagliaButton").text().trim().split(" ");
+            var activeSize = size[0];
+            var activeSizeQty = parseInt(size[3]);
+            var idProd = $_GET.get('productId');
+            var idCat = $_GET.get('categoryId');
+            var data;
+            var ajaxurl = '../php/order.php';
+            if(activeSizeQty==0){
+                showSnackBar("Taglia e colore scelto non disponibile");
+            }
+            else{
+                data =  {'color': activeColor,'size': activeSize, 'idProdotto': parseInt(idProd), "idCategoria": parseInt(idCat)};
+                $.post(ajaxurl, data).done(function() {showSnackBar("Oggetto aggiunto correttamente al carrello")});
+                addMessage(activeColor,activeSize);
+                size[3]=activeSizeQty-1;
+                $(".tagliaButton").text(size.join().replace(/,/g," "));
+                flag=0;
+            }
+            $.ajax({url: "../php/result.json", success: function(result){
+                console.log(result);
+                if(result > 0) {
+                    console.log(!$("a.cart").children().hasClass("badge"));
+                    if(!$("a.cart").children().hasClass("badge")) {
+                        $("a.cart").append(`<span class="badge"></span>`);
+                    }
+                    if(!$("#messaggi span.icon_A_nav").children().hasClass("badge")) {
+                        $("#messaggi span.icon_A_nav").append(`<span class="badge"></span>`);
+                    }
+                }
+            }});
         }
         else{
-            data =  {'color': activeColor,'size': activeSize, 'idProdotto': parseInt(idProd), "idCategoria": parseInt(idCat)};
-            $.post(ajaxurl, data).done(function() {showSnackBar("Oggetto aggiunto correttamente al carrello")});
-            addMessage(activeColor,activeSize);
-            cont++;
+            showSnackBar("Taglia non scelta");
         }
-        $.ajax({
-            type: "POST",
-            url: '../php/order.php',
-            data: data,
-            success: function (){
-                showSnackBar();
-                addMessage(activeColor,activeSize);
-            }
-          });
-        $.ajax({url: "../php/result.json", success: function(result){
-            console.log(result);
-            if(result > 0) {
-                console.log(!$("a.cart").children().hasClass("badge"));
-                if(!$("a.cart").children().hasClass("badge")) {
-                    $("a.cart").append(`<span class="badge"></span>`);
-                }
-                if(!$("#messaggi span.icon_A_nav").children().hasClass("badge")) {
-                    $("#messaggi span.icon_A_nav").append(`<span class="badge"></span>`);
-                }
-            }
-        }});
     });
     $("article header ul li").click(function () {
         $("article header > div").empty().append($(this).html());
@@ -70,9 +67,9 @@ $(document).ready(function(){
     });
     $(".size li").click(function () {
         if(!$(this).is(":first-of-type")) {
-            $("button:first-of-type").text($(this).text());
+            $(".tagliaButton").text($(this).text());
         }
-        $(this).children().last().attr("checked", "checked");
+        flag=1;
         $(".size").css("display", "none");
     });
 });
